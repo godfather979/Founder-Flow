fa3b28cd13d601c776712db0728300583699006a
godfather979 <thakkerved5@gmail.com>
2025-02-15 19:51:56 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import './App.css';
+import "./App.css";
 import "@fontsource/poppins"; // Headings
 import "@fontsource/inter"; // Body text
 import "@fontsource/montserrat"; // Buttons
+import NavInv from "./components/Navbar";
 
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
-
 function App() {
-
-
-
   return (
     <>
-    <BrowserRouter>
-      <RoutesWeb />
-    </BrowserRouter>
+      <BrowserRouter>
+        <RoutesWeb />
+      </BrowserRouter>
     </>
   );
 }
@@ -23,12 +20,13 @@ const RoutesWeb = () => {
   const location = useLocation(); // Get the current route
 
   return (
-      <>
+    <>
       {/* {loading && <Loader />} */}
       <Routes>
+        <Route path="/nav" element={<NavInv />} />
       </Routes>
       {/* {!isAdminRoute && <Footer />}       Conditionally render Footer */}
-      </>
+    </>
   );
 };
 
