5d65a423f71c30645458d991f393927a57e69d7b
Mihit10 <mihitsingasane@gmail.com>
2025-02-16 05:36:25 +0530

    marketing

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import "@fontsource/inter"; // Body text
 import "@fontsource/montserrat"; // Buttons
 import NavHome from "./pages/nav/NavHome";
 import NavLegal from "./pages/nav/NavLegal";
+import NavMarketing from "./pages/nav/NavMarketing";
+
 import NavIdea from "./pages/nav/NavIdea";
 import NavIdeaGen from "./pages/nav/NavIdeaGen";
 import NavIdeaVal from "./pages/nav/NavIdeaVal";
 import NavRoadMap from "./pages/nav/NavRoadMap";
-import IdeaGenerator from "./pages/IdeaGenerator";
-import IdeaValidator from "./pages/IdeaValidator";
-import RoadMap from "./pages/RoadMap";
 import Landing from "./pages/Landing";
 
 
@@ -41,6 +40,7 @@ const RoutesWeb = () => {
         <Route path="/ideation/RoadMap" element={<NavRoadMap />} />
         
         <Route path="/legal" element={<NavLegal />} />
+        <Route path="/marketing" element={<NavMarketing />} />
 
       </Routes>
       {/* {!isAdminRoute && <Footer />}       Conditionally render Footer */}
